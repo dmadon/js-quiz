83ea5b56af66620b2f641f101b3943a21cef1b8b
dmadon <deanna.madon@gmail.com>
2022-06-17 23:11:09 -0500

    updated html with placeholders for question info

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@ startButtonEl=document.querySelector("#start-button");
 timerEl=document.querySelector("#timer");
 startTextEl=document.querySelector("#start-text");
 mainEl=document.querySelector("#main");
-buttonEl=document.querySelector(".answer-button");
+answer1El=document.querySelector("#ans1");
+questionEl = document.querySelector("#questionHolder");
+questionTextEl = document.querySelector("#questionText");
+answer1El = document.querySelector("#ans1");
+answer2El = document.querySelector("#ans2");
+answer3El = document.querySelector("#ans3");
+answer4El = document.querySelector("#ans4");
 
 
 
@@ -10,92 +16,81 @@ questions=[
     {questionId: 1,
     question:  "This is question 1",
     ans1: "question 1, answer 1",
-    ans1rs: "correct",
     ans2: "question 1, answer 2",
-    ans2rs: "wrong",
     ans3: "question 1, answer 3",
-    ans3rs: "wrong",
     ans4: "question 1, answer 4",
-    ans4rs: "wrong"
+    correct: "ans1"
     },
     {questionId: 2,
     question:  "This is question 2",
     ans1: "question 2, answer 1",
-    ans1rs: "correct",
     ans2: "question 2, answer 2",
-    ans2rs: "wrong",
     ans3: "question 2, answer 3",
-    ans3rs: "wrong",
     ans4: "question 2, answer 4",
-    ans4rs: "wrong"
+    correct: "ans1"
     },
     {questionId: 3,
     question:  "This is question 3",
     ans1: "question 3, answer 1",
-    ans1rs: "correct",
     ans2: "question 3, answer 2",
-    ans2rs: "wrong",
     ans3: "question 3, answer 3",
-    ans3rs: "wrong",
     ans4: "question 3, answer 4",
-    ans4rs: "wrong"
+    correct: "ans1"
     },
 ];
 
 
 
 
+
+
+
     var createQuestion = function(event){
         
+
         
-        for (i = 0; i <= questions.length-1; i++){
+        for (i=0; i <= questions.length-1; i++){
+
+        
 
             var target = event.target;
             var targetId = (target.id);
             console.log(targetId);
             startTextEl.className="hidden";
-
-
-            var questionEl = document.createElement("div");
-            questionEl.className="question";
-            questionEl.innerHTML = "<p>"+JSON.stringify(questions[i].question).slice(1,-1)+"</p>";
-            mainEl.appendChild(questionEl);
-
-            var answer1El = document.createElement("button");
-            answer1El.className="ansBtn1";
-            answer1El.id="ansBtn1";
-            answer1El.innerHTML=JSON.stringify(questions[i].ans1).slice(1,-1);
-            questionEl.appendChild(answer1El);
-            button1El=document.querySelector(".ansBtn1"); 
-            button1El.addEventListener("click",createQuestion);
-
-            var answer2El = document.createElement("button");
-            answer2El.className="ansBtn2";
-            answer2El.id="ansBtn2";
-            answer2El.innerHTML=JSON.stringify(questions[i].ans2).slice(1,-1);
-            questionEl.appendChild(answer2El);
-            button2El=document.querySelector(".ansBtn2");
-            button2El.addEventListener("click",createQuestion);
             
-            var answer3El = document.createElement("button");
-            answer3El.className="ansBtn3";
-            answer3El.id="ansBtn3";
-            answer3El.innerHTML=JSON.stringify(questions[i].ans3).slice(1,-1);
-            questionEl.appendChild(answer3El);
-            button3El=document.querySelector(".ansBtn3");
-            button3El.addEventListener("click",createQuestion);
             
-            var answer4El = document.createElement("button");
-            answer4El.className="ansBtn4";
-            answer4El.id="ansBtn4";
-            answer4El.innerHTML=JSON.stringify(questions[i].ans4).slice(1,-1);
-            questionEl.appendChild(answer4El);
-            button4El=document.querySelector(".ansBtn4");
-            button4El.addEventListener("click",createQuestion);
-      
+            questionEl.setAttribute("data-question-id",i);
+          
+            questionTextEl.textContent =JSON.stringify(questions[i].question).slice(1,-1);
+            
+            answer1El.className="answer-button";
+            answer1El.textContent=JSON.stringify(questions[i].ans1).slice(1,-1);
+            answer1El.addEventListener("click",createQuestion);
 
+  
+            answer2El.className="answer-button";
+            answer2El.textContent=JSON.stringify(questions[i].ans2).slice(1,-1);
+            // // button2El=document.querySelector(".ansBtn2");
+            // // button2El.addEventListener("click",clickResponse);
+            
+        
+            answer3El.className="answer-button";
+            answer3El.textContent=JSON.stringify(questions[i].ans3).slice(1,-1);
+            // // button3El=document.querySelector(".ansBtn3");
+            // // button3El.addEventListener("click",clickResponse);
+            
+         
+            answer4El.className="answer-button";
+            answer4El.textContent=JSON.stringify(questions[i].ans4).slice(1,-1);
+            // // button4El=document.querySelector(".ansBtn4");
+            // // button4El.addEventListener("click",clickResponse);
 
-          break;
+            
+            console.log(i);
+
+            break;
+
+        
         
 
         }
